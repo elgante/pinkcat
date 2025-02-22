510986bc5dd69f7f98e03412b7225ab6fc5677b6
PinkCatCodes <elgante.ce@gmail.com>
2025-02-22 18:35:01 +0100

    meditation screen details ui

--- src/app/meditation/[id].tsx
+++ src/app/meditation/[id].tsx
@@ -1,7 +1,12 @@
-import { Text } from "react-native";
+import { Text, View, Pressable } from "react-native";
 import { router, useLocalSearchParams } from "expo-router";
 import { meditations } from "@/meditation-data";
 import AntDesign from "@expo/vector-icons/AntDesign";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
+import Slider from "@react-native-community/slider";
+
 import {
   SafeAreaView,
   useSafeAreaInsets,
@@ -10,24 +15,70 @@ import {
 export default function MeditationDetails() {
   const { id } = useLocalSearchParams<{ id: string }>();
 
-  const { top } = useSafeAreaInsets();
-
   const meditation = meditations.find((m) => m.id === Number(id));
 
   if (!meditation) {
     return <Text className="text-xl">Meditation not found</Text>;
   }
   return (
-    <SafeAreaView>
-      <Text className="text-xl mt-10">{meditation?.title}</Text>
-      <AntDesign
-        onPress={() => router.back()}
-        className="absolute right-4"
-        style={{ top: top + 16 }}
-        name="close"
-        size={24}
-        color="black"
-      />
+    <SafeAreaView className="bg-rose-200 flex-1 p-2">
+      {/* HEADER */}
+      <View className=" flex-1">
+        <View className=" flex-1">
+          <View className=" flex-row items-center justify-between p-10">
+            <AntDesign name="infocirlceo" size={16} color="black" />
+
+            <View className="bg-gray-900 p-2 rounded-md">
+              <Text className="text-zinc-100 font-semibold">
+                Today's meditation
+              </Text>
+            </View>
+
+            <AntDesign
+              onPress={() => router.back()}
+              name="close"
+              size={24}
+              color="black"
+            />
+          </View>
+          <Text className="text-xl mt-10 text-center font-semibold">
+            {meditation?.title}
+          </Text>
+        </View>
+
+        {/* PLAY/PAUSE button */}
+        <Pressable className="bg-gray-500 self-center w-20 aspect-square rounded-full items-center justify-center">
+          <FontAwesome5 name="play" size={24} color="snow" />
+        </Pressable>
+
+        {/* FOOTER: player  */}
+        <View className=" flex-1">
+          <View className="p-5">
+            <View className="flex-row justify-between ">
+              <MaterialIcons name="airplay" size={24} color="black" />
+              <MaterialCommunityIcons name="cog" size={24} color="black" />
+            </View>
+
+            {/* Playback */}
+            <View>
+              <Slider
+                style={{ width: "100%", height: 40 }}
+                value={0.5}
+                onSlidingComplete={(value) => console.log(value)}
+                minimumValue={0}
+                maximumValue={1}
+                minimumTrackTintColor="#FFFFFF"
+                maximumTrackTintColor="#000000"
+              />
+            </View>
+
+            <View className="flex-row justify-between">
+              <Text>03:24</Text>
+              <Text>13:00</Text>
+            </View>
+          </View>
+        </View>
+      </View>
     </SafeAreaView>
   );
 }
